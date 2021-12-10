aae83144b0eb60283cbfec195fb855f73c797091
SanZhang <three@sanwu.ink>
2021-12-10 20:32:30 +0800

    1

---
+++ Sprite.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type IDisplayObject interface {
+	SetVisible(NewVisible bool)
+	RemoveFromParent()
+}
+type IDisplayContainer interface {
+	AddChild()
+	RemoveChild()
+}
+type TObject struct {
+}
+type TActor struct {
+	TObject
+}
+
+func (a *TActor) BeginPlay() {
+	fmt.Println("Begin Play")
+}
+
+type TPawn struct {
+	TActor
+}
+
+type TCharacter struct {
+	TPawn
+}
